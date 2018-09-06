51d34d722a33dd62c270b61dd3082535bdd3682e
zhangshoukang <zhangshoukang@fancydigital.com.cn>
2018-09-06 10:18:18 +0800

    create my project datastructure

---
+++ src/main/java/com/zsk/Arrays/ArrayBasic.java
@@ -0,0 +1,10 @@
+package com.zsk.Arrays;
+
+/**
+ * Create by zsk on 2018/9/6
+ **/
+public class ArrayBasic {
+    public static void main(String[] args) {
+        System.out.println("hello data structure");
+    }
+}
